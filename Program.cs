caaa6faa5d0f9bca1bf2141f047d97ceeabd5bb9
alaa <alaa@DESKTOP-10U3HBM>
2024-09-05 06:30:17 +0300

    as

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("hello alaa mamdouh");
+            Console.WriteLine("asssssssssssssssssssssssssssssss"); Console.WriteLine("ggggggggg");
         }
     }
 }
